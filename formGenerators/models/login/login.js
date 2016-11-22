f408e58354b6f9745e05e362db32c0b91fc6bee8
nirabhratapaswi <nirabhratapaswi@gmail.com>
2016-11-22 11:59:22 +0530

    logging in user model added

---
+++ formGenerators/models/login/login.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose'),
+	Login = require('./model'),
+	conn = 'mongodb://localhost:27017/login';
+
+mongoose.connect(conn, function(error) {
+	if (error)
+		throw error;
+
+	console.log('Successfully connected to MongoDB');
+});
+
+/*Login.findOne({
+	username: 'user1'
+}, function(error, user) {
+	if (error)
+		return console.error(error);
+
+	user.validatePassword('user1', function(error, valid) {
+		if (error)
+			console.error(error);
+
+		console.log("user1 : " + valid);
+	});
+
+	user.validatePassword('user2', function(error, valid) {
+		if (error)
+			console.error(error);
+
+		console.log("user2 : " + valid);
+	});
+
+});*/
+
+function saveUser(user) {
+	var newUser = new Login({
+		username: user.username,
+		password: user.password
+	});
+
+	newUser.save(function(error, user) {
+		if(error) 
+			return console.error(error)
+
+		console.log("User saved successfully: " + user.username);
+	});
+}
+
+module.exports = {
+	saveUser = saveUser
+}
--- formGenerators/routes/login/index.js
+++ formGenerators/routes/login/index.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+var express = require('express'),
+	router = express.Router(),
+	path = require('path'),
+	login = require("/../../models/login/login");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login/index');
+	res.render('login/index');
 });
 
 router.post('/', function(req, res, next) {
-	console.log("Post made!!");
-	console.log(req.body.username);
-	console.log(req.body.pwd);
+	var user {
+		username: req.body.username,
+		password: req.body.pwd
+	}
+	login.saveUser(user);
 	res.end("Successful submission!");
 });
 
 router.get('/index2', function(req, res, next) {
-  res.render('login/index2');
+	res.render('login/index2');
 });
 
 router.get('/index3', function(req, res, next) {
-  res.render('login/index3');
+	res.render('login/index3');
 });
 
 module.exports = router;
